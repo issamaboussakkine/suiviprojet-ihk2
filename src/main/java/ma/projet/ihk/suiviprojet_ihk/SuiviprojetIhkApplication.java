aa6df0b1137bcf1569f4ec09c31eb934afb70e12
issamaboussakkine <issamaboussakkine@gmail.com>
2026-03-12 23:47:52 +0000

    init: initialisation du projet Spring Boot - structure de base

---
+++ src/main/java/ma/projet/ihk/suiviprojet_ihk/SuiviprojetIhkApplication.java
@@ -0,0 +1,13 @@
+package ma.projet.ihk.suiviprojet_ihk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviprojetIhkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviprojetIhkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/projet/ihk/suiviprojet_ihk/SuiviprojetIhkApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.projet.ihk.suiviprojet_ihk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiviprojetIhkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
